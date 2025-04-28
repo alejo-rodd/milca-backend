d74de9fc326c0c1a02a50f8585cb35ae98a0b695
alejo-rodd <alejorodg@outlook.com>
2025-04-27 20:16:29 -0500

    udpate(app): update app module and main

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,46 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Logger, Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { CommonModule } from './common/common.module';
+import { ColorsModule } from './colors/colors.module';
+import { CollectionsModule } from './collections/collections.module';
+import { MugsModule } from './mugs/mugs.module';
+import { AuthModule } from './auth/auth.module';
+import { PhotosCatalogModule } from './photos-catalog/photos-catalog.module';
+import { PriceHistoryModule } from './price-history/price-history.module';
+import { DiscountsModule } from './discounts/discounts.module';
+import { ClientsModule } from './clients/clients.module';
+import { PurchaseOrdersModule } from './purchase-orders/purchase-orders.module';
+import { ImagesModule } from './images/images.module';
+
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +(process.env.DB_PORT ?? 5432),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      ssl: true,
+      autoLoadEntities: true,
+      synchronize: true,
+    }),
+
+    CommonModule,
+    ColorsModule,
+    CollectionsModule,
+    MugsModule,
+    AuthModule,
+    PhotosCatalogModule,
+    PriceHistoryModule,
+    DiscountsModule,
+    ClientsModule,
+    PurchaseOrdersModule,
+    ImagesModule,
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,32 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const logger = new Logger('Bootstrap');
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    })
+  );
+
+  const config = new DocumentBuilder()
+    .setTitle('Milca RESTFul API')
+    .setDescription('Milca endpoints')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  const port = process.env.PORT ? +process.env.PORT : 3000;
+  await app.listen(port);
+  logger.log(`App running on port ${port}`);
 }
 bootstrap();
